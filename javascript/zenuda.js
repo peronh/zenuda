05941f1636a93d4708c0c1fa70746dd67e30ec9a
pronyecz <pronyecz@gmail.com>
2018-03-16 13:10:44 +0100

    update btn top html & css

--- javascript/zenuda.js
+++ javascript/zenuda.js
@@ -44,6 +44,30 @@ $(document).ready(function() {
         $("#menu-nav").css({"width": "0", "transition":"width 500ms", "-webkit-transition":"width 500ms", "z-index":"3000"});
         $("body").css({"margin-right": "0" , "overflow":"visible"});   
     });
+
+     $('.btntop').on("click", function() {
+
+            $('html,body').animate( {
+                scrollTop: 0
+            }, 700);
+        }
+    );
+
+    $(document).on("scroll", function() {
+
+        if($(document).scrollTop()>600) {
+            $(".btntop").show(250);
+        }
+        else {
+            $(".btntop").hide(250);
+        }
+    });
+
+    var targetWidth=1400;
+    $('.desktop').on('click', function() {
+            $('meta[name="viewport"]').attr('content', 'width='+targetWidth);
+        }
+    );
    
 
 });
